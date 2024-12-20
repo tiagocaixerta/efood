6c9bbd52ef9f3fbf0f90f0bdcccfc6abe1bbb04a
Tiago <caixerta63@gmail.com>
2024-12-20 10:07:02 -0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import GlobalStyle, { Container } from './styles';
-import { BrowserRouter } from 'react-router-dom';
-import Rotas from './routes';
-import { Provider } from 'react-redux';
-import { store } from './store';
-import Cart from './components/Cart';
+import React from 'react'
+import GlobalStyle, { Container } from './styles'
+import { BrowserRouter } from 'react-router-dom'
+import Rotas from './routes'
+import { Provider } from 'react-redux'
+import { store } from './store'
+import Cart from './components/Cart'
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
         <Cart />
       </BrowserRouter>
     </Provider>
-  );
+  )
 }
 
-export default App;
+export default App
